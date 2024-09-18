c460a46fbcac5add47a699050372ae1741040a9b
Udani Fernando <udanif@fixel.lk>
2024-09-18 15:58:54 +0530

    Initial Commit

---
+++ Trail Blaze/Trail_BlazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trail_BlazeApp.swift
+//  Trail Blaze
+//
+//  Created by Udani Fernando on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Trail_BlazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
